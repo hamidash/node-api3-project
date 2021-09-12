f8210f8224efdff53666c37ad8dd5fbd6aab5ce8
Hamida Sh <sharifovahamida@gmail.com>
2021-09-11 22:56:45 -0700

    comment nodemon

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // require your server and launch it
+require('dotenv').config();
+
+
 const server = require('./api/server');
 
-const port = 5000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
